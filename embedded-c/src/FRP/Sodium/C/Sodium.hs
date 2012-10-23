a3abe0c090b4901e25e0f2559004406ccf4ccb82
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2012-10-23 23:06:53 +1300

    Some hacking on the embedded C implementation.

--- embedded-c/src/FRP/Sodium/C/Sodium.hs
+++ embedded-c/src/FRP/Sodium/C/Sodium.hs
@@ -7,7 +7,7 @@ import Control.Applicative
 import Control.Monad.State.Strict
 import Data.ByteString (ByteString)
 import Data.Int
-import Data.IntMap
+import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 import Data.Maybe
 import Data.Word
@@ -23,8 +23,16 @@ data Value = Value {
     }
 
 class RType a where
+    ctype :: a -> CTypeSpec
     r :: Value -> a
     unr :: a -> Value
+
+konstant :: forall a . (RNum a, Integral a) => a -> R a
+konstant i = r $ Value {
+        vaDecls = [],
+        vaStats = [],
+        vaExpr  = castedNumeric (ctype (undefined :: R a)) i
+    }
 
 class RType (R a) => RNum a where
     data R a :: *
@@ -38,92 +46,74 @@ class RType (R a) => RNum a where
       where a = unr ra
             b = unr rb
 
-castedNumeric :: Integral i => String -> i -> CExpr
+castedNumeric :: Integral i => CTypeSpec -> i -> CExpr
 castedNumeric typ i = CCast
-                        (CDecl [CTypeSpec (CTypeDef (Ident typ 0 undefNode) undefNode)] [] undefNode)
+                        (CDecl [CTypeSpec typ] [] undefNode)
                         (CConst (CIntConst (CInteger (fromIntegral i) DecRepr noFlags) undefNode))
                         undefNode
 
 instance RNum Int64 where
     newtype R Int64 = RInt64 Value
-    constant i = RInt64 $ Value {
-            vaDecls = [],
-            vaStats = [],
-            vaExpr  = castedNumeric "int64_t" i
-        }
+    constant = konstant
 instance RType (R Int64) where
+    ctype _ = CTypeDef (Ident "int64_t" 0 undefNode) undefNode
     r = RInt64
     unr (RInt64 v) = v
 
 instance RNum Int32 where
     newtype R Int32 = RInt32 Value
-    constant i = RInt32 $ Value {
-            vaDecls = [],
-            vaStats = [],
-            vaExpr  = castedNumeric "int32_t" i
-        }
+    constant = konstant
 instance RType (R Int32) where
+    ctype _ = CTypeDef (Ident "int32_t" 0 undefNode) undefNode
     r = RInt32
     unr (RInt32 v) = v
 
 instance RNum Int16 where
     newtype R Int16 = RInt16 Value
-    constant i = RInt16 $ Value {
-            vaDecls = [],
-            vaStats = [],
-            vaExpr  = castedNumeric "int16_t" i
-        }
+    constant = konstant
 instance RType (R Int16) where
+    ctype _ = CTypeDef (Ident "int16_t" 0 undefNode) undefNode
+    r = RInt16
     unr (RInt16 v) = v
 
 instance RNum Int8 where
     newtype R Int8 = RInt8 Value
-    constant i = RInt8 $ Value {
-            vaDecls = [],
-            vaStats = [],
-            vaExpr  = castedNumeric "int8_t" i
-        }
+    constant = konstant
 instance RType (R Int8) where
+    ctype _ = CTypeDef (Ident "int8_t" 0 undefNode) undefNode
+    r = RInt8
     unr (RInt8 v) = v
 
 instance RNum Word64 where
     newtype R Word64 = RWord64 Value
-    constant i = RWord64 $ Value {
-            vaDecls = [],
-            vaStats = [],
-            vaExpr  = castedNumeric "uint64_t" i
-        }
+    constant = konstant
 instance RType (R Word64) where
+    ctype _ = CTypeDef (Ident "uint64_t" 0 undefNode) undefNode
+    r = RWord64
     unr (RWord64 v) = v
 
 instance RNum Word32 where
     newtype R Word32 = RWord32 Value
-    constant i = RWord32 $ Value {
-            vaDecls = [],
-            vaStats = [],
-            vaExpr  = castedNumeric "uint32_t" i
-        }
+    constant = konstant
 instance RType (R Word32) where
+    ctype _ = CTypeDef (Ident "uint32_t" 0 undefNode) undefNode
+    r = RWord32
     unr (RWord32 v) = v
 
 instance RNum Word16 where
     newtype R Word16 = RWord16 Value
-    constant i = RWord16 $ Value {
-            vaDecls = [],
-            vaStats = [],
-            vaExpr  = castedNumeric "uint16_t" i
-        }
+    constant = konstant
 instance RType (R Word16) where
+    ctype _ = CTypeDef (Ident "uint16_t" 0 undefNode) undefNode
+    r = RWord16
     unr (RWord16 v) = v
 
 instance RNum Word8 where
     newtype R Word8 = RWord8 Value
-    constant i = RWord8 $ Value {
-            vaDecls = [],
-            vaStats = [],
-            vaExpr  = castedNumeric "uint8_t" i
-        }
+    constant = konstant
 instance RType (R Word8) where
+    ctype _ = CTypeDef (Ident "uint8_t" 0 undefNode) undefNode
+    r = RWord8
     unr (RWord8 v) = v
 
 data Behavior a = Behavior Int
@@ -135,19 +125,50 @@ data EventImpl where
     Code :: String -> CStat -> EventImpl
 
 data ReactiveState = ReactiveState {
+        rsInputType :: CTypeSpec,
         rsInputs    :: [Int],
         rsNextIdent :: String,
         rsNextEvent :: Int,
         rsEvents    :: IntMap [EventImpl]
     }
 
-newReactiveState :: ReactiveState
-newReactiveState = ReactiveState {
+newReactiveState :: CTypeSpec -> ReactiveState
+newReactiveState typ = ReactiveState {
+        rsInputType = typ,
         rsInputs    = [],
         rsNextIdent = "a",
         rsNextEvent = 2,
         rsEvents    = IM.empty
     }
+
+toC :: ReactiveState -> [CExternalDeclaration NodeInfo]
+toC rs = [
+        CFDefExt $ CFunDef [
+                CTypeSpec (CVoidType undefNode)
+            ]
+            (
+                CDeclr (Just $ Ident "banana" 0 undefNode) [
+                        CFunDeclr (Right ([
+                            CDecl [
+                                CTypeSpec $ rsInputType rs
+                            ] [(Just (
+                                CDeclr (Just $ Ident "apple" 0 undefNode) [] Nothing [] undefNode
+                            ), Nothing, Nothing)] undefNode
+                        ], False)) [] undefNode
+                    ] Nothing [] undefNode
+            )
+            []
+            (
+                CCompound [] stmts undefNode
+--                    CBlockStmt (CBreak undefNode)
+            )
+            undefNode
+    ]
+  where
+    stmts = flip concatMap (IM.toList (rsEvents rs)) $ \(ix, impls) ->
+        flip concatMap impls $ \impl ->
+            case impl of
+                _ -> []
 
 newtype Reactive a = Reactive { unReactive :: State ReactiveState a }
     deriving (Functor, Applicative, Monad)
@@ -191,13 +212,13 @@ listen :: Event a
        -> ByteString -- ^ Statement or statement block to handle it
        -> Reactive ()
 listen (Event ea) ident statement = do
-    case execParser statementP statement (position 0 "" 1 1) [] [] of
+    case execParser statementP statement (position 0 "blah" 1 1) [Ident "hello" 0 undefNode] (map Name [0..]) of
         Left err -> fail $ "parse failed in listen: " ++ show err
         Right (stmt, _) -> do
             connect ea (Code ident stmt)
 
-react :: (Event a -> Reactive ()) -> ReactiveState
-react r = execState (unReactive (r (Event 1))) newReactiveState
+react :: forall a . RType a => (Event a -> Reactive ()) -> ReactiveState
+react r = execState (unReactive (r (Event 1))) (newReactiveState (ctype (undefined :: a)))
 
 main :: IO ()
 main = do
@@ -205,5 +226,5 @@ main = do
             eb <- mapE (`plus` constant (1 :: Int32)) ea
             listen eb "x" "{ printf(\"x=%d\\n\", x); }"
             return ()
-    return ()
+    putStrLn $ show $ pretty $ CTranslUnit (toC st) undefNode
 
