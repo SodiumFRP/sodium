506ed0cb2aa74e8b790e74d3050b988aa74db424
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2012-12-11 10:18:29 +1300

    Make Listener into a monoid so we can combine listeners together.

--- java/sodium/src/sodium/Event.java
+++ java/sodium/src/sodium/Event.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Event<A> {
-	private static final class ListenerImplementation<A> implements Listener {
+	private static final class ListenerImplementation<A> extends Listener {
 		/**
 		 * It's essential that we keep the listener alive while the caller holds
 		 * the Listener, so that the finalizer doesn't get triggered.
--- java/sodium/src/sodium/Listener.java
+++ java/sodium/src/sodium/Listener.java
@@ -3,6 +3,22 @@ package sodium;
 import java.util.List;
 import java.util.ArrayList;
 
-public interface Listener {
-    void unlisten();
+public class Listener {
+    public Listener() {}
+
+    public void unlisten() {}
+
+    /**
+     * Combine listeners into one where a single unlisten() invocation will unlisten
+     * both the inputs.
+     */
+    public Listener append(final Listener l) {
+        return new Listener() {
+            public void unlisten() {
+                this.unlisten();
+                l.unlisten();
+            }
+        };
+    }
 }
+
