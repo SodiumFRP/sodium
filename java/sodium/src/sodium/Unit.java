6296e42d9ea2b6303e08f3dc4f2da65617638df4
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2014-07-09 19:46:22 +1200

    Add equality to Unit.

--- java/sodium/src/sodium/Unit.java
+++ java/sodium/src/sodium/Unit.java
@@ -3,5 +3,9 @@ package sodium;
 public class Unit {
     public static final Unit unit = new Unit();
     Unit() {}
-};
 
+    @Override
+    public boolean equals(Object other_) {
+        return other_ instanceof Unit;
+    }
+};
