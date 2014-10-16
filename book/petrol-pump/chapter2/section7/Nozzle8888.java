090413ac8c821d0a16902f0d13aba7e1acb3e41f
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2014-10-16 23:04:19 +1300

    Book: Remove the assignment to a variable in Nozzle8888.java

--- book/petrol-pump/chapter2/section7/Nozzle8888.java
+++ book/petrol-pump/chapter2/section7/Nozzle8888.java
@@ -10,10 +10,12 @@ public class Nozzle8888 implements Pump
         Behavior<UpDown> nozzle1 = inputs.eNozzle1.hold(UpDown.DOWN);
         Behavior<UpDown> nozzle2 = inputs.eNozzle2.hold(UpDown.DOWN);
         Behavior<UpDown> nozzle3 = inputs.eNozzle3.hold(UpDown.DOWN);
-        Lambda1<UpDown, String> ifLifted8 =
-            u -> u.equals(UpDown.UP) ?  "8.8.8.8." : "";
-        return new Outputs().setPriceLCD1(nozzle1.map(ifLifted8))
-                            .setPriceLCD2(nozzle2.map(ifLifted8))
-                            .setPriceLCD3(nozzle3.map(ifLifted8));
+        return new Outputs()
+            .setPriceLCD1(nozzle1.map(u ->
+                u.equals(UpDown.UP) ?  "8.8.8.8." : ""))
+            .setPriceLCD2(nozzle2.map(u ->
+                u.equals(UpDown.UP) ?  "8.8.8.8." : ""))
+            .setPriceLCD3(nozzle3.map(u ->
+                u.equals(UpDown.UP) ?  "8.8.8.8." : ""));
     }
 }
