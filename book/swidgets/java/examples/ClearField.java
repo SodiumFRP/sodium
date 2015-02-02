dfd12643511c070277e6d508e9d7fd839d8af3bf
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2015-02-03 00:57:45 +1300

    Tidy up Book example "ClearField"

--- book/swidgets/java/examples/ClearField.java
+++ book/swidgets/java/examples/ClearField.java
@@ -6,27 +6,21 @@ import sodium.*;
 
 public class ClearField {
     public static void main(String[] args) {
-        JFrame view = new JFrame("ClearField") {
-            public Dimension getPreferredSize() {
-                return new Dimension(400, 160);
-            }
-        };
+        JFrame view = new JFrame("ClearField");
         view.setLayout(new FlowLayout());
 
-        Transaction.runVoid(() -> {
-            SButton clear = new SButton("Clear");
-            Stream<String> sClearIt = clear.sClicked.map(u -> "");
-            STextField text = new STextField(sClearIt, "Hello", 15);
-            view.add(text);
-            view.add(clear);
-        });
+        SButton clear = new SButton("Clear");
+        Stream<String> sClearIt = clear.sClicked.map(u -> "");
+        STextField text = new STextField(sClearIt, "Hello", 15);
+        view.add(text);
+        view.add(clear);
 
         view.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
-        view.pack();
+        view.setSize(400, 160);
         view.setVisible(true);
     }
 }
