0f30777db5a299df028e2962f9b8193f4e12f686
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2015-02-04 02:40:38 +1300

    Rx.JS example for the book.

---
+++ book/whiteboard/server.js
@@ -0,0 +1,38 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) { });
+server.listen(1433, function() { });
+
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+var clients = [];
+
+wsServer.on('request', function(request) {
+    var conn = request.accept(null, request.origin);
+    clients.push(conn);
+    console.log(clients.length+" clients");
+
+    conn.on("message", function(message) {
+        try {
+            for (var i = 0; i < clients.length; i++) {
+                if (clients[i] !== conn)
+                    clients[i].send(message.utf8Data);
+            }
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+    });
+
+    conn.on("close", function (code, reason) {
+        for (var i = 0; i < clients.length; i++)
+            if (clients[i] === conn) {
+                clients.splice(i, 1);
+                break;
+            }
+        console.log(clients.length+" clients");
+    });
+});
