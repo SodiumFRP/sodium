baf68eef45c6498b1122c24e06d14596ae86ed81
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2015-10-12 18:26:12 +1300

    Whack that mole! example

---
+++ book/web/whack.js
@@ -0,0 +1,150 @@
+function mkMole(id, clock, sClick, x, y)
+{
+    var tRise = 100;
+    var tWhack = 15;
+    var tUp = 500;
+    function drawFace(ctx, x, y, color) {
+        ctx.beginPath();
+        ctx.arc(x,y,20,0,2*Math.PI);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(x - 10, y - 1); ctx.lineTo(x - 5, y - 3);
+        ctx.moveTo(x + 5, y - 3);  ctx.lineTo(x + 10, y - 1);
+        ctx.stroke();
+    }
+    function drawMole(ctx, x, y, up, fracVisible) {
+        if (up)
+            drawFace(ctx, x, y, '#dfdf00');
+        else {
+            ctx.save();
+            ctx.beginPath();
+            ctx.rect(x - 21, y - 21, 42, 42);
+            ctx.clip();
+            drawFace(ctx, x, y + 40 - fracVisible * 40, '#cf7f00');
+            ctx.restore();
+            ctx.beginPath();
+            ctx.moveTo(x - 26, y + 21);
+            ctx.lineTo(x + 26, y + 21);
+            ctx.stroke();
+        }
+    }
+    var state = new Rx.BehaviorSubject({ phase : 'rising', t0 : 0 }),
+        sUp = clock.withLatestFrom(state,
+                function (t, state) {
+                    return state.phase == 'rising' && t >= tRise
+                       ? { phase : 'up', t0 : t }
+                       : null;
+                })
+            .filter(function (state) { return state !== null; });
+        sWhack = sClick.withLatestFrom(clock, state,
+                function (_, t, state) {
+                    return state.phase == 'rising'
+                        ? { phase : 'whacked',
+                            t0 : t - (1 - t / tRise) * tWhack }
+                        : null;
+                })
+            .filter(function (state) { return state !== null; });
+    sUp.merge(sWhack).subscribe(state);
+    var drawable = new Rx.BehaviorSubject(function(_) { });
+    clock.withLatestFrom(state, function (t, state) {
+            return state.phase == 'rising' ? function (ctx) {
+                       drawMole(ctx, x, y, false, t / tRise); } :
+                   state.phase == 'up' ? function (ctx) {
+                       drawMole(ctx, x, y, true, 1); } :
+                   function (ctx) {
+                       var dt = t - state.t0;
+                       if (dt < tWhack)
+                           drawMole(ctx, x, y, false,
+                               1 - dt / tWhack); };
+        }).subscribe(drawable);
+    var sDestroy = clock
+            .withLatestFrom(state,
+                function (t, st) {
+                    var dur = t - st.t0;
+                    return (st.phase == 'up' && dur >= tUp)
+                        || (st.phase == 'whacked' && dur >= tWhack)
+                                     ? id : null;
+                })
+            .filter(function (id) { return id != null; });
+    return {
+        id : id,
+        drawable : drawable,
+        sDestroy : sDestroy
+    };
+}
+
+function init() {
+    var canvas = document.getElementById("myCanvas"),
+        getXY = function(e) {
+            return { x : e.pageX - canvas.offsetLeft,
+                     y : e.pageY - canvas.offsetTop }; },
+        sMouseDown = Rx.Observable.fromEvent(canvas, 'mousedown')
+                                  .map(getXY),
+        clock = new Rx.BehaviorSubject(0);
+    Rx.Observable.interval(20).subscribe(clock);
+    var state = new Rx.BehaviorSubject({ nextID : 0, moles : []}),
+        sAddMole = clock
+            .filter(function (_) { return Math.random() < 0.02; })
+            .withLatestFrom(state, clock,
+                function (_, state, t0) {
+                    var x = 25+(canvas.width-50) * Math.random();
+                    var y = 25+(canvas.height-50) * Math.random();
+                    var sClick = sMouseDown.filter(function (pt) {
+                        return pt.x >= x - 20 && pt.x <= x + 20 &&
+                               pt.y >= y - 20 && pt.y <= y + 20;
+                    });
+                    var newMoles = state.moles.slice();
+                    newMoles.push(mkMole(state.nextID,
+                        clock.map(function (t) { return t - t0; }),
+                        sClick, x, y));
+                    state = { nextID : state.nextID+1,
+                              moles : newMoles };
+                    console.log("add mole "+state.nextID+
+                        " ("+state.moles.length+")");
+                    return state;
+                }),
+        sDestroy = state.flatMapLatest(
+            function (state) {
+                var sDestroy = Rx.Observable.of();
+                for (var i = 0; i < state.moles.length; i++)
+                    sDestroy = sDestroy.merge(state.moles[i].sDestroy);
+                return sDestroy;
+            });
+        sRemoveMole = sDestroy.withLatestFrom(state,
+            function (id, state) {
+                var newMoles = [];
+                for (var i = 0; i < state.moles.length; i++)
+                    if (state.moles[i].id != id)
+                        newMoles.push(state.moles[i]);
+                console.log("remove mole "+id+" ("+newMoles.length+")");
+                return { nextID : state.nextID, moles : newMoles };
+            });
+    sAddMole.merge(sRemoveMole).subscribe(state);
+    var drawables = new Rx.BehaviorSubject([]);
+    state.flatMapLatest(
+        function (state) {
+            var drawables = new Rx.BehaviorSubject([]);
+            for (var i = 0; i < state.moles.length; i++) {
+                var thiz = state.moles[i].drawable.map(
+                        function(draw) {
+                            return [draw];
+                        });
+                drawables = i == 0
+                    ? thiz
+                    : drawables.withLatestFrom(thiz,
+                        function (d1, d2) { return d1.concat(d2); });
+            }
+            return drawables;
+        }).subscribe(drawables);
+    clock.subscribe(function(t) {
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle = '#00af00';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        var ds = drawables.getValue();
+        for (var i = 0; i < ds.length; i++)
+            ds[i](ctx);
+    });
+}
+
