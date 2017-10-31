9058e096405d8807512061a85d695abec26d9307
jam40jeff <jam40jeff@gmail.com>
2017-10-31 12:24:29 -0400

    Added CleanupNow method to Cleanup.

--- c#/src/Sodium/Sodium/Cleanup.cs
+++ c#/src/Sodium/Sodium/Cleanup.cs
@@ -10,10 +10,19 @@ namespace Sodium
         // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
         private readonly Stream<Unit> stream;
 
+        private readonly Action cleanup;
+
         public Cleanup(Action cleanup)
         {
+            this.cleanup = cleanup;
+            
             this.stream = Stream.Never<Unit>();
             this.stream.AttachListener(Listener.Create(cleanup));
         }
+
+        /// <summary>
+        /// Force the cleanup to happen now rather than waiting for this object to be garbage collected.
+        /// </summary>
+        public void CleanupNow() => this.cleanup();
     }
 }
