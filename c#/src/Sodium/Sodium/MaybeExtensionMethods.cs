26669be326313803ccff36c4bf38a102c1a9494e
jam40jeff <jam40jeff@gmail.com>
2018-09-12 10:56:32 -0400

    Add AllMaybeOrNone method.

--- c#/src/Sodium/Sodium/MaybeExtensionMethods.cs
+++ c#/src/Sodium/Sodium/MaybeExtensionMethods.cs
@@ -12,5 +12,14 @@ namespace Sodium
             .Select(m => m.Match(v => (Value: v, HasValue: true), () => (Value: default(T), HasValue: false)))
             .Where(p => p.HasValue)
             .Select(p => p.Value);
+
+        public static Maybe<IEnumerable<T>> AllMaybeOrNone<T>(this IEnumerable<Maybe<T>> o)
+        {
+            (T Value, bool HasValue)[] rr = (o ?? new Maybe<T>[0])
+                .Select(m => m.Match(v => (Value: v, HasValue: true), () => (Value: default(T), HasValue: false)))
+                .ToArray();
+
+            return rr.Any(r => !r.HasValue) ? Maybe.None : Maybe.Some(rr.Select(r => r.Value));
+        }
     }
 }
