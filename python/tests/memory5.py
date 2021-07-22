f7113ca0e0db58a6ac6fb3002bb63c0da77eb13e
Andrey Krainyak <mode.andrew@gmail.com>
2021-07-22 21:12:01 +0800

    Port MemoryTest5 test

---
+++ python/tests/memory5.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import sys
+from threading import Thread
+from time import sleep
+import tracemalloc as tm
+
+sys.path.append(str(Path(__file__).parent.parent))
+from sodiumfrp.core import Cell, StreamSink
+
+def main() -> None:
+
+    def thread() -> None:
+        tm.start()
+        while True:
+            current, peak = tm.get_traced_memory()
+            print(f"memory current: {current}, peak: {peak}")
+            sleep(5)
+    Thread(target=thread).start()
+
+    eChange: StreamSink[int] = StreamSink()
+    out: Cell[int] = eChange.hold(0)
+    l = out.listen(lambda tt: None)
+    for i in range(0, 1000000000):
+        eChange.send(i)
+    l.unlisten()
+
+if __name__ == "__main__":
+    main()
