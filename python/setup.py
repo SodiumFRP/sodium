734a6cc20f9775a672bce5a777b3b50c14ed3179
Andrey Krainyak <mode.andrew@gmail.com>
2022-04-20 23:09:19 +0800

    Initial setup.py
    Required for distribution as a package.

---
+++ python/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+def get_long_description():
+    with open("README.md", "r") as readme_file:
+        return readme_file.read()
+
+setuptools.setup(
+    name="sodiumfrp",
+    version="1.0.0",
+    author="Stephen Blackheath",
+    description= \
+        "Python implementation of Sodium - "
+        "Functional Reactive Programming (FRP) library",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SodiumFRP/sodium",
+    packages=setuptools.find_packages(),
+    license="BSD-3 License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
